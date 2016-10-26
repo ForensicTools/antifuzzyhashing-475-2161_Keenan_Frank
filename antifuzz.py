89c3b869a3b86c36adc0c52da3a48ead14cbb00e
Ryan Frank <rlf3208@rit.edu>
2016-10-25 21:46:32 -0400

    change to use python wrapper for ssdeep. hash orginal and copy and compare the two hashes

--- antifuzz.py
+++ antifuzz.py
@@ -8,45 +8,31 @@ Authors: Kaitlin Keenan and Ryan Frank
 import sys
 from shutil import copy2
 import subprocess
-
+import ssdeep #http://python-ssdeep.readthedocs.io/en/latest/installation.html
 
 def main():
-
 
 	# Take in file
 	ogFile = sys.argv[1]
 
-
 	# Make copy of file
 	newFile = sys.argv[2]
-
 	copy2(ogFile, newFile)
 
+	ogHash = ssdeep.hash_from_file(ogFile)
+	newHash = ssdeep.hash_from_file(newFile)
 
-	print "\n"
+	print ogHash
+	print ssdeep.compare(ogHash, newHash)
 
-
-	# Run ssdeep on files
-	hashesFile = open('hashes.txt', 'w+')
-
-	p = subprocess.Popen(['ssdeep', '-b', ogFile, hashesFile], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-	
+"""
+def cmd(command):
+	#if (arg2 && arg1):
+	p = subprocess.Popen(command.split(), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 	out, err = p.communicate()
-
-	hashesFile.write(out.decode('utf-8'))
-
-	p2 = subprocess.Popen(['ssdeep', '-bm', hashesFile, newFile], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-
-	out2, err2 = p2.communicate()
-
-	hashesFile.close()
-
-	print out2 #+ "\n"
-
-	#print err2
-
-
-
+	print err
+	return out
+"""
 
 if __name__ == "__main__":
 	main()
