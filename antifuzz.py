03c7961564fef6a4228841c19db583b62c7d14e0
Bill Stackpole <bill.stackpole@gmail.com>
2016-11-03 11:17:16 -0400

    attempt to resolve conflicts
    <fingers crossed 2>

--- antifuzz.py
+++ antifuzz.py
@@ -4,11 +4,11 @@ File:  antifuzz.py
 Authors: Kaitlin Keenan and   Ryan Frank
 
 '''
+#Authors: Kaitlin Keenan and   Ryan Frank
 
 import sys
 from shutil import copy2
 import subprocess
-
 
 def main():
 
@@ -47,6 +47,34 @@ def main():
 
 
 
+=======
+import ssdeep #http://python-ssdeep.readthedocs.io/en/latest/installation.html
+
+def main():
+
+	# Take in file
+	ogFile = sys.argv[1]
+
+	# Make copy of file
+	newFile = sys.argv[2]
+
+	# Mess with the given file
+	cmd(['lame','--quiet', '--scale', '1', ogFile])
+	print cmd(['mv', ogFile + ".mp3", newFile])
+
+	# Hash files
+	ogHash = ssdeep.hash_from_file(ogFile)
+	newHash = ssdeep.hash_from_file(newFile)
+
+	# Compare the hashes
+	#print ogHash
+	print ssdeep.compare(ogHash, newHash)
+
+def cmd(command):
+	#if (arg2 && arg1):
+	p = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+	out, err = p.communicate()
+	return out
 
 if __name__ == "__main__":
 	main()
