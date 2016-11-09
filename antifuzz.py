e5a7ce5aca5a8bc96217ef74fb2f9b072a9dfb84
Ryan Frank <rlf3208@rit.edu>
2016-11-09 12:41:09 -0500

    Add input checking. Changed the arguments. Move original hash to before antifuzzing

--- antifuzz.py
+++ antifuzz.py
@@ -2,8 +2,6 @@
 File:  antifuzz.py
 
 Authors: Kaitlin Keenan and Ryan Frank
-
-#
 '''
 
 import sys
@@ -16,26 +14,38 @@ def main():
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument("originalFile", help="File to antifuzz")
-	parser.add_argument("newFile", help="Name of the antifuzzed file")
+	parser.add_argument("--newFile", help="Name of the antifuzzed file")
 	args = parser.parse_args()
+	pattern = re.compile('mp3$')
 
+	if args.newFile is None:
+		args.newFile = args.originalFile
+	if not args.originalFile.endswith('.mp3'):
+		print "Please use a file with the .mp3 extension for your original file"
+		return 1
+	if not args.newFile.endswith('.mp3'):
+		print "Please use a file with the .mp3 extension for your newfile"
+		return 1
+	
 	# Take in file
 	ogFile = args.originalFile
 
 	# Make copy of file
 	nFile = args.newFile
 
+	ogHash = ssdeep.hash_from_file(ogFile)
 	# Mess with the given file
 	mp3(ogFile, nFile)
 
 	# Hash files
-	ogHash = ssdeep.hash_from_file(ogFile)
 	newHash = ssdeep.hash_from_file(nFile)
 
 	# Compare the hashes
 	#print ogHash
 	diff=str(ssdeep.compare(ogHash, newHash))
-	print("The files are " + diff + "% different")
+	print("The files are " + diff + "% similar")
+
+	return 0
 
 def mp3(ogFile, newFile):
 	cmd(['lame','--quiet', '--scale', '1', ogFile])
